a52d429f7d2080b91e51f5ab4ff99ae04b871dbe
DeanKamen <41880178+DeanKamen@users.noreply.github.com>
2022-02-03 09:43:49 -0600

    maybe not all

--- OpenCL-HUBERT/constant_headers/softmax_sf_layer0.h
+++ OpenCL-HUBERT/constant_headers/softmax_sf_layer0.h
@@ -1,6 +1,6 @@
 #ifndef SOFTMAX_SF_LAYER0_H
 #define SOFTMAX_SF_LAYER0_H
 
-extern float softmax_sf_layer0[1] = {0.0011662038};
+float softmax_sf_layer0[1] = {0.0011662038};
 
 #endif // SOFTMAX_SF_LAYER0_H

b0021d672db5261870c4e5a193a983dd1e6f4013
DeanKamen <41880178+DeanKamen@users.noreply.github.com>
2022-03-28 09:51:35 -0500

    GELU compiles on quartus

--- OpenCL-HUBERT/gelu.cpp
+++ OpenCL-HUBERT/gelu.cpp
@@ -2,6 +2,7 @@
 #include "HLS/hls.h"
 #include "HLS/stdio.h"
 #include "tensors.h" 
+#include "tensor3d.h"
 #include <iostream>
 #include "quantact.h"
 #include "hubertEnums.h"
@@ -11,7 +12,7 @@
 #include <float.h>
 
 //verification
-#include "constant_headers/gelu_pre_fte.h"
+//#include "constant_headers/gelu_pre_fte.h"
 //member functions
 Tensor3d int_erf(gelu_memory memory, Tensor3d x_int, const int xr, const int xc, const int xd) {
 	//x_int is normally 22x1x3072, while our scaling factor constants are 1x3072
@@ -24,11 +25,11 @@ Tensor3d int_erf(gelu_memory memory, Tensor3d x_int, const int xr, const int xc,
 
 	mul_dot(memory.y_int, xr, xc, xd, memory.sign, xr,xc,xd, memory.y_int);
 	div_scalar(memory.y_int, xr, xc, xd, 1 << 14, memory.y_int);//This generates a small amount of error when compared to python verions
-	eq(memory.y_int,xr,xc,xd, (Tensor)gelu_pre_fte, xr, xc, xd);
+	//eq(memory.y_int,xr,xc,xd, (Tensor)gelu_pre_fte, xr, xc, xd);
 	//TODO skipped floor_ste????
 	return memory.y_int;
 }
-Tensor3d gelu_forward(gelu_memory memory, Tensor3d x_int, const int xr, const int xc, const int xd) {
+component Tensor3d gelu_forward(gelu_memory memory, Tensor3d x_int, const int xr, const int xc, const int xd) {
 	memory.sigmoid_int = int_erf(memory, x_int, xr, xc, xd);
 	add(memory.sigmoid_int,xr,xc,xd, memory.shift_int, memory.sfr, memory.sfc, memory.sigmoid_int);
 	mul_dot(x_int, xr, xc, xd, memory.sigmoid_int, xr, xc, xd, x_int);
